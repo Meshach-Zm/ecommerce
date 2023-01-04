d28217070e3c339770425891da51deec2d716200
Meshachzm <63595721+Meshachzm@users.noreply.github.com>
2023-01-04 01:47:16 -0800

    Update [id].ts

--- pages/api/p/[id].ts
+++ pages/api/p/[id].ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import {prisma} from "../../lib/prisma";
+import prisma from '../../client'
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
 
