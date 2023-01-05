f5606aedebcf6e2c6d9809b6d0cdf4f9bb0071bf
Meshach-Zm <117562274+Meshach-Zm@users.noreply.github.com>
2023-01-04 22:48:00 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,7 @@ const Home  = ({allproducts}) => {
 
         <div className={styles.grid}>
           {
-              allproducts.map((item: any) => {
+              allproducts?.map((item: any) => {
            return( 
                   <div className={styles.card} key={item.id}>
                   <Products productData={item}    />
