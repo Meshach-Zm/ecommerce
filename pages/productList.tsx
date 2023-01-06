c0e087f11ca7a4f63e2bc2dc0fb743f3427617b2
Meshach-Zm <117562274+Meshach-Zm@users.noreply.github.com>
2023-01-06 00:03:25 -0800

    Update productList.tsx

--- pages/productList.tsx
+++ pages/productList.tsx
@@ -8,16 +8,16 @@ const Products = ({productData}) => {
 
   return (
     <>
-     <div key={productData?.id}>  
+     <div key={productData.id}>  
               <Image src={productData?.image} width={250} height={300} alt="product Data" />
               <h3>{productData?.name}</h3>
               <p>{productData?.description}</p>
               <h4 >Prices</h4> <hr className={styles.hr}/>
               <div className={styles.prices}>
-                <p>K{productData?.prices.shoprite} Shoprite</p>
-                <p>K{productData?.prices.game} Game</p>
-                <p>K{productData?.prices.picknpay} PickNPay</p> 
-                <p>K{productData?.prices.vml} VML</p>
+                <p>K{productData.prices.shoprite} Shoprite</p>
+                <p>K{productData.prices.game} Game</p>
+                <p>K{productData.prices.picknpay} PickNPay</p> 
+                <p>K{productData.prices.vml} VML</p>
               </div>
           </div>
           <button type="submit" onSubmit={contactData} className="">Buy</button>
